8eba89f2fcda5bfe210d04e9f369c7f53e937811
Kittoru <mama190774@gmail.com>
2024-09-19 18:07:26 +0300

    Add new project

--- src/shared/model/constants.js
+++ src/shared/model/constants.js
@@ -1,9 +1,9 @@
 import Simple from '../assets/simple.jpg';
 import Coca from '../assets/coca.jpg';
-import Store from '../assets/fashion.jpg';
 import Weather from '../assets/weather.jpg';
+import Todo from '../assets/todo.jpg';
 
-export const arrSkills = ['HTML', 'CSS', 'SCSS', 'TailwindCSS', 'JavaScript', 'API', 'ReactJS', 'React Router v6', 'Git', 'Figma', 'FSD', 'методология БЭМ'];
+export const arrSkills = ['HTML', 'CSS', 'SASS', 'TailwindCSS', 'JavaScript', 'ReactJS', 'React Router v6', 'Redux Toolkit', 'Git', 'Figma', 'FSD', 'БЭМ'];
 
 export const arrProjects = [
   {
@@ -24,18 +24,18 @@ export const arrProjects = [
   },
   {
     id: 3,
-    img: Store,
-    title: 'Store',
-    tech: 'ReactJS, ReactRouter, TailwindCSS, FSD',
-    descriprion: 'SPA',
-    link: 'https://kittoru.github.io/Store/',
-  },
-  {
-    id: 4,
     img: Weather,
     title: 'Weather App',
     tech: 'ReactJS, FSD, API',
     descriprion: 'SPA',
     link: 'https://kittoru.github.io/Weather-App/',
+  },
+  {
+    id: 4,
+    img: Todo,
+    title: 'Todo',
+    tech: 'ReactJS, FSD, Redux Toolkit, SASS',
+    descriprion: 'SPA, адаптив',
+    link: 'https://kittoru.github.io/Todo-List/',
   },
 ];
